5b9ca960159c8c22f4a1133884b1dcd5365dbe72
ValKiriann <insanityanni@gmail.com>
2020-06-16 11:46:22 +0200

    added expedition service tests

---
+++ app/tests/services/expedition.service.test.js
@@ -0,0 +1,33 @@
+const expeditionService = require('../../services/expedition.service');
+
+describe('Verify Params: Verify Terrain is valid', () => {
+    test('The service recieves the terrain object and checks if if all the coordinates are set', () => {
+        return expect(() => {
+            return expeditionService.verifyTerrain(undefined);
+        }).toThrow();
+    });
+
+    test('Missing Coordinates', () => {
+        return expect(() => {
+            return expeditionService.verifyTerrain({x:3});
+        }).toThrow();
+    });
+
+    test('Nan Coordinates', () => {
+        return expect(() => {
+            return expeditionService.verifyTerrain({x:"xy", y:"yx"});
+        }).toThrow();
+    });
+
+    test('Maximum coordinate value of 50', () => {
+        return expect(() => {
+            return expeditionService.verifyTerrain({x:70, y:85});
+        }).toThrow();
+    });
+
+    test('Valid terrain', () => {
+        return expect(() => {
+            return expeditionService.verifyTerrain({x:3, y:5});
+        }).toBeTruthy();
+    });
+});
