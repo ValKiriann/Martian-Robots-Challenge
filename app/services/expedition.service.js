2ff0346d19d0967c0d209ac57e3b3bc802c7fd22
ValKiriann <insanityanni@gmail.com>
2020-06-16 11:47:58 +0200

    quick fix, return true when terrain is validated for code coherence

--- app/services/expedition.service.js
+++ app/services/expedition.service.js
@@ -2,4 +2,5 @@ exports.verifyTerrain = (terrain) => {
     if(!terrain || !terrain.x || !terrain.y || isNaN(terrain.x) || isNaN(terrain.y) || terrain.x > 50 || terrain.y > 50) {
         throw {statusCode:400, errorCode: "Verify Params Error", errorData: "Please enter a valid terrain"}
     }
+    return true;
 }
